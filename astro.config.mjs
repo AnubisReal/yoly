5182381a675d9f30085dbcc65323177152e6ccee
AnubisReal <yoanmanuel1998@gmail.com>
2026-01-28 16:33:26 +0100

    3ra

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yolyoly.com'
+	site: 'https://yolyoly.com',
 	
 	server: {
 		host: '0.0.0.0',
